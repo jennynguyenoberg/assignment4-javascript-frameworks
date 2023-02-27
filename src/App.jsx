d5d3a5dc5cfc54f5c791bb44e4c013cc3314d186
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-02-27 02:23:54 +0100

    clean up code + remove folder

--- src/App.jsx
+++ src/App.jsx
@@ -63,24 +63,6 @@ function App() {
       });
   };
 
-  function App() {
-    const [show, setShow] = useState(true);
-
-    function changeState() {
-      setShow(!show);
-    }
-
-    return (
-      <div className="App">
-        {show ? (
-          <button onClick={changeState}> Display </button>
-        ) : (
-          <button onClick={changeState}> Hide </button>
-        )}
-      </div>
-    );
-  }
-
   return (
     <div className="main-container">
 
